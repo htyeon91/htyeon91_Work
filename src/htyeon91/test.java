f9894d1bf68282277562613e4fa0f63d461d27f5
hwang <htyeon91@naver.com>
2014-06-09 21:27:52 +0900

    큰식

---
+++ src/htyeon91/test.java
@@ -0,0 +1,15 @@
+
+package htyeon91;
+import java.*;
+import java.util.Scanner;
+import java.util.*;
+
+public class test
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("장근식 노답");
+	}
+
+}
